130795147fb025ed2b8ad8c560eec95d248c1e2c
Adebayo Temitope <adecancode@gmail.com>
2021-05-02 19:48:41 +0100

    +

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const ejs = require('ejs')
 const app = express()
 const cors = require("cors")
 require("dotenv").config()
-const PORT = process.env.PORT || 9000
+const port = process.env.PORT || 9000
 
 app.use(express.json());
 app.use(cors());
