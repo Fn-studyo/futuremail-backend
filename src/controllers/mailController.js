dbfd910f0861061c86e9108aacc5aabefaf76e1b
Adebayo Temitope <adecancode@gmail.com>
2021-05-05 19:25:26 +0100

    +

--- src/controllers/mailController.js
+++ src/controllers/mailController.js
@@ -4,7 +4,7 @@ const ejs = require('ejs')
 require("dotenv").config()
 
 const transporter = nodemailer.createTransport({
-    // host: process.env.EMAIL_HOST,
+    host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: true, //ssl
     auth: {
